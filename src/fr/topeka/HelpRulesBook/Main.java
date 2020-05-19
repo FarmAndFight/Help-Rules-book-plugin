7b6f2845df1277bce36ee8cba8282bf9734be837
Quentin Legot <quentin3157.github@gmail.com>
2020-05-19 20:45:33 +0200

    Initial commit

---
+++ src/fr/topeka/HelpRulesBook/Main.java
@@ -0,0 +1,17 @@
+package fr.topeka.HelpRulesBook;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
